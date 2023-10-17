0d8667b454cadb3d159db8125bb46c0236a5073e
mattweedy <matthewtweedy02@gmail.com>
2023-10-18 00:35:06 +0100

    added all relevant html, beginning styling and scripting requirements

---
+++ problem-1/scripts.js
@@ -0,0 +1,17 @@
+// ------------------ validate()
+// check all validation asked for
+// if fails any, reveal the 'error' div
+// if all passes, call addContact()
+
+// addContact()
+// find the element for the contact list
+// create new element with the stored and cleaned input
+
+// filter()
+// figure this one out
+// when name is clicked it will first go by asc, followed by desc
+
+// searchMobile()
+// have it find each char input and add it to a string
+// continuously be looking for matches in list
+// if none, reveal 'noResult' div
