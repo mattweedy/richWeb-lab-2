25b3e75d85e7a2157f9cdd9304cb857a7b4dfd85
mattweedy <matthewtweedy02@gmail.com>
2023-10-18 17:20:17 +0100

    working on testing functions to add contacts

--- problem-1/scripts.js
+++ problem-1/scripts.js
@@ -1,9 +1,51 @@
-// ------------------ validateForm()
+// dom elements
+const contactAddButton = document.getElementById("contact-add");
+const error = document.getElementById("error");
+const noResult = document.getElementById("noResult");
+
+let contacts = [];
+
+// set error and noResult to invisible
+error.style.display = "none";
+noResult.style.display = "none";
+
+// listener
+contactAddButton.addEventListener("click", validateForm);
+
+function validateForm() {
+    // get the form inputs
+    // console.log("test");
+
+    const name = document.getElementById("contact-name").value;
+    const mobile = document.getElementById("contact-mobile").value;
+    const email = document.getElementById("contact-email").value;
+
+    if (
+        name.trim() !== "" &&
+        mobile.trim() !== "" &&
+        email.trim() !== ""
+    ) {
+        console.log(name)
+        console.log(mobile)
+        console.log(email)
+    } else {
+        error.style.display = "block";
+        noResult.style.display = "block";
+    }
+
+    // name.match(/^[A-Za-z ]{1,20}$/)
+    addContact(name, mobile, email);
+
+}
 // check all validation asked for
 // if fails any, reveal the 'error' div
 // if all passes, call addContact()
 
-// addContact()
+function addContact(name, mobile, email) {
+    // GRAB THE TEXT AND ADD A NEW ROW, CAN FULLY VAlidate after
+    const contactTable = document.getElementById("contact-table");
+    const constTBody = contactTable.querySelector("tbody");
+}
 // find the element for the contact list
 // create new element with the stored and cleaned input
 
